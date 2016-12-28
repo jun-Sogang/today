8185b3c974c7160d84509d5d1b9f245e9e005638
jun-Sogang <Sogang.jun.developer@gmail.com>
2016-12-28 19:25:59 +0900

    change .js

---
+++ 16.DEC/1228.js
@@ -0,0 +1,20 @@
+//Definition of Closure
+
+//스코프에 있지 않은 것을 사용할 수 있게 하는 것
+//클로져는 스코프에 있지 않은 변수를 가리키는 하나 하나를 의미한다.
+
+
+
+//Prototype
+var obj2 = {
+x : 2
+};
+
+var obj1 = Object.create(obj2);
+obj1.a = obj1.x;
+
+//obj.x 는 obj2에 가서 x값을 불러온다.
+
+obj1.x = obj1.x;
+
+//first obj1.x는 obj1에 x라는 키를 가지게 하고 second obj1.x는 obj2.x에 가서 접근한다
